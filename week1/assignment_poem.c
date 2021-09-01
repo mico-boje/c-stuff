07890804a6061f2ae914d98a0a6bb8ecc31eeb91
mico-boje <mico-boje@live.dk>
2021-09-01 17:05:50 +0200

    week 1 completed

---
+++ week1/assignment_poem.c
@@ -0,0 +1,14 @@
+/*
+code to print a poem
+*/
+
+#include<stdio.h>
+
+int main(void) {
+    printf("With the sun in my hand\n"
+            "Gonna throw the sun\n"
+            "Way across the land-\n"
+            "Cause I’m tired,\n"
+            "Tired as I can be\n");
+            return 0;
+}
