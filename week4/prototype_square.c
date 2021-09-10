694e4ea40b215e849a4678a4558fbee561a82863
mico-boje <mico.boeje@gmail.com>
2021-09-10 21:02:21 +0200

    Added cube function

--- week4/prototype_square.c
+++ week4/prototype_square.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 double square(double);
+double cube(double);
 
 int main(void)
 {
@@ -11,8 +12,8 @@ int main(void)
 
     for (i = 1; i < how_many; i++){
         for (j = 0; j <= 10; j++){
-            printf("\n %lf \t %lf",
-                i+(j/10.0), square(i+(j/10.0)));
+            printf("\n %lf \t %lf \t %lf",
+                i+(j/10.0), square(i+(j/10.0)), cube(i+(j/10.0)));
         }
     }
     printf("\n\n");
@@ -22,4 +23,9 @@ int main(void)
 double square(double x)
 {
     return (x * x);
+}
+
+double cube(double x)
+{
+    return(x*x*x);
 }
